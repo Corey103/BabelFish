bdae92069e1e749e06a8037d498d040a6089cde2
Corey103 <Coreyenon@gmail.com>
2018-04-16 12:12:48 -0400

    Added button support, still needs quiz setup. Half of menus setup. No images added yet.

---
+++ app/src/main/java/com/example/corey/androidstudioproject/PlayGame.java
@@ -0,0 +1,40 @@
+package com.example.corey.androidstudioproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class PlayGame extends AppCompatActivity {
+
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_game);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        button = findViewById(R.id.button5);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+    }
+    public void StartGame(View v){
+        Intent StartGame = new Intent(this, StartGame.class);
+        startActivity(StartGame);
+    }
+
+}
